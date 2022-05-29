5315e4cb859de6031aca97fac899719f61b0d55a
ubermetengo <93676034+ubermetengo@users.noreply.github.com>
2022-05-29 20:39:45 +0300

    Add files via upload

---
+++ string_methods.py
@@ -0,0 +1,8 @@
+message = ' Hello, my name is Çağrı ŞİMŞEK.'
+print(message)
+print(message.upper()) #tüm harfleri büyük yapar .upper()
+print(message.lower()) #tüm hargleri küçük yapar .lower()
+print(message.title()) #her bir kelimenin ilk harfini büyük yapar .title
+print(message.capitalize()) #tümce düzeni, ilk harfi büyük yapar .capitalize
+print(message.strip()) #.strip baştaki ve sondaki karakterleri siler.
+print(message.split()) #.split her bir string diziyi boşluklarından ayırarak böler, bi nevi bölme
